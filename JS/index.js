3f1df14e71470a9907ab9035fd2804fa1dad5974
niButera <nikolasxp683@gmail.com>
2024-01-31 19:51:28 -0300

    fiz o basico do basico

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+// Adicione eventos de clique aos ícones
+document.getElementById("homeIcon").addEventListener("click", function () {
+    alert("Você já está na pagina inicial");
+  });
+
+document.getElementById("historicoIcon").addEventListener("click", function () {
+    alert("Clicou em Histórico");
+});
+
+document.getElementById("relatorioIcon").addEventListener("click", function () {
+    alert("Clicou em Relatório");
+});
+
+document.getElementById("perfilIcon").addEventListener("click", function () {
+    alert("Clicou em Perfil");
+});
